c0313c0b50e5042772a3b4c2039215bf71d495c8
max <scraptrashscrap@gmail.com>
2021-03-21 09:16:21 +0100

    Done

--- src/com/practice/X.java
+++ src/com/practice/X.java
@@ -1,4 +1,7 @@
 package com.practice;
+
+import java.util.Scanner;
+
 // Write a small program to read an integer from the keyboard
 // (using Scanner) and print out the times table for that number.
 // The table should run from 1 to 12.
@@ -17,9 +20,21 @@ package com.practice;
 // Optional Extra:
 // Change your program so that ALL variables (including the scanner
 // instance) are called x.
-public class Main {
 
+public class X {
+    private static int x;
+
+    // I cannot use the instance fields from the static context (because I cannot use static method via instance)!!!
     public static void main(String[] args) {
-	// write your code here
+        Scanner x = new Scanner(System.in);
+        System.out.println("Please a number that you want to multiply in the range of 1-12");
+        X.x = x.nextInt();
+        X.x();
+    }
+
+    public static void x() {
+        for(int x = 1; x <= 12; x++) {
+            System.out.println(x * X.x);
+        }
     }
 }
